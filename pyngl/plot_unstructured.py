be443e2ccff91e5f63569874ee97fb2d43a33ef4
Benjamin Hillman <benjaminr.hillman@gmail.com>
2020-02-21 08:56:18 -0700

    Make gridfile optional for pyngl plot_unstructured

--- pyngl/plot_unstructured.py
+++ pyngl/plot_unstructured.py
@@ -6,7 +6,7 @@ import numpy
 import xarray
 
 def plot_unstructured(
-        xv, yv, data, plot_format='x11',
+        x, y, data, plot_format='x11',
         plot_name='dummy', **kwargs):
 
     # Setup the canvas
@@ -17,9 +17,16 @@ def plot_unstructured(
     res.cnFillOn = True
     res.cnLinesOn = False
     res.cnFillPalette = 'MPL_viridis'
-    res.cnFillMode = 'CellFill'
-    res.sfXCellBounds = xv
-    res.sfYCellBounds = yv
+
+    # If passed 2d coordinate arrays assume they represent cell vertices, 
+    # otherwise assume cell centers
+    if len(x.shape) == 2:
+        res.cnFillMode = 'CellFill'
+        res.sfXCellBounds = x
+        res.sfYCellBounds = y
+    else:
+        res.sfXArray = x
+        res.sfYArray = y
 
     # Tweak plot appearance
     res.mpGridAndLimbOn = False
@@ -36,17 +43,28 @@ def plot_unstructured(
     return plot
 
 
-def main(datafile, gridfile, varname):
+def main(datafile, varname, gridfile=None):
+
     # Read data
     ds_data = xarray.open_dataset(datafile)
-    ds_grid = xarray.open_dataset(gridfile).rename({'grid_size': 'ncol'})
     data = ds_data[varname]
-    lon_edges = ds_grid['grid_corner_lon']
-    lat_edges = ds_grid['grid_corner_lat']
+    if gridfile is not None:
+        ds_grid = xarray.open_dataset(gridfile).rename({'grid_size': 'ncol'})
+        lon = ds_grid['grid_corner_lon']
+        lat = ds_grid['grid_corner_lat']
+    else:
+        lon = ds_data['lon']
+        lat = ds_data['lat']
+
+    # Make sure we don't have time or level dimensions
+    if 'time' in data.dims:
+        data = data.isel(time=0).squeeze()
+    if 'lev' in data.dims:
+        data = data.isel(lev=-1).squeeze()
 
     # Make plot
     plot = plot_unstructured(
-        lon_edges.values, lat_edges.values, data.values,
+        lon.values, lat.values, data.values,
         plot_format='png', plot_name=varname, 
         mpGeophysicalLineColor='white',
         lbOrientation='horizontal', 
