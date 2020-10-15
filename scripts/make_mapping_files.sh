c02fc428f6af19c2bc144f4fb6ad6b3e7fc402b4
Benjamin Hillman <bhillma@sandia.gov>
2020-10-15 11:39:34 -0700

    Reorganize files

---
+++ scripts/make_mapping_files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Output root
+output_root=${SCRATCH}/grids
+mkdir -p ${output_root}
+
+# Model grid
+model_name="ne1024pg2"
+model_grid="${output_root}/ne1024pg2_scrip_fix.nc"
+echo "Generate model_grid file ${model_grid}"
+if [ ! -e ${model_grid} ]; then
+    GenerateCSMesh --alt --res 1024 --file ${output_root}/ne1024.g
+    GenerateVolumetricMesh --in ${output_root}/ne1024.g --out ${output_root}/ne1024pg2.g --np 2 --uniform
+    ConvertExodusToSCRIP --in ${output_root}/ne1024pg2.g --out ${model_grid}
+fi
+
+# Obs grids
+obs_name="ceres1deg"
+obs_file="/global/cfs/cdirs/e3sm/terai/Obs_datasets/CERES/CERES_SYN1deg-3H_Terra-Aqua-MODIS_Ed4.1_Subset_20200101-20200229.nc"
+obs_grid="${output_root}/ceres_scrip.nc"
+echo "Generate obs_grid file ${obs_grid}"
+if [ ! -e ${obs_grid} ]; then
+    ncks --rgr infer --rgr scrip=${obs_grid} ${obs_file} ~/foo.nc
+fi
+
+# Generate mapping file
+map_file="${output_root}/map_${model_name}_to_${obs_name}_nco.nc"
+if [ ! -e ${map_file} ]; then
+    echo "Generate mapping file ${map_file}"
+    ncremap --fl_fmt=64bit_data --alg_typ=nco --src_grd=${model_grid} --dst_grd=${obs_grid} -m ${map_file}
+fi
+
+
+obs_name="era5"
+obs_file="/global/cfs/cdirs/e3sm/terai/Obs_datasets/ERA5/ERA5_surf_20200101_20200229.nc"
+obs_grid="${output_root}/era5_scrip.nc"
+echo "Generate obs_grid file ${obs_grid}"
+if [ ! -e ${obs_grid} ]; then
+    ncks --rgr infer --rgr scrip=${obs_grid} ${obs_file} ~/foo.nc
+fi
+
+method="aave"
+map_file="${output_root}/map_${model_name}_to_${obs_name}_${method}.nc"
+if [ ! -e ${map_file} ]; then
+    echo "Generate mapping file ${map_file}"
+    ncremap --fl_fmt=64bit_data --alg_typ=${method} --src_grd=${model_grid} --dst_grd=${obs_grid} -m ${map_file}
+fi
---
+++ scripts/make_ne1024_analysis.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+from glob import glob
+import sys, os
+import xarray
+import ngl
+import numpy
+from e3smplot.e3sm_utils import area_average
+from e3smplot.pyngl import compare_maps
+
+# List of fields we want to make map plots for
+map_vars = (
+    'FSNTOA',
+    'FSNTOAC',
+    'FLNT',
+    'FLNTC',
+    #'PRECT',
+    'CLDTOT',
+    #'SHFLX',
+    #'TREFHT',
+)
+
+# List of fields we want to make global mean time series for
+time_series_vars = map_vars
+
+obs_sources = {
+    'FSNTOA' : 'CERES-SYN',
+    'FSNTOAC': 'CERES-SYN',
+    'FLNT'   : 'CERES-SYN',
+    'FLNTC'  : 'CERES-SYN',
+    'PRECT'  : 'GPM',
+    'CLDTOT' : 'CERES-SYN',
+    'SHFLX'  : 'ERA5',
+    'TREFHT' : 'ERA5',
+}
+
+# Offline mapping weights from model to obs sources
+map_files = {
+    'CERES-SYN': '/global/cscratch1/sd/bhillma/grids/map_ne1024pg2_to_ceres1deg_nco.nc',
+    'GPM': None,
+    'ERA5': None,
+}
+
+test_case_name = 'SCREAM-HR'
+test_data_path = '/global/cscratch1/sd/terai/e3sm_scratch/cori-knl/master.ne1024pg2_r0125_oRRS18to6v3.F2010-SCREAM-HR-DYAMOND2.cori-knl_intel.1536x8x16.DY2_Oct9.20201009-16/run'
+obs_data_path = {
+    'CERES-SYN': '/global/cfs/cdirs/e3sm/terai/Obs_datasets/CERES',
+    'GPM': None,
+    'ERA5': None,
+}
+graphics_root = './graphics'
+os.makedirs(graphics_root, exist_ok=True)
+
+for vname in map_vars:
+
+    # Only processed CERES data so far
+    if obs_sources[vname] is not 'CERES-SYN': continue
+    print(f'Processing {vname}...')
+
+    # Find files
+    test_files = sorted(glob(f'{test_data_path}/*.h1.*.nc'))
+    obs_files = sorted(glob(f'{obs_data_path[obs_sources[vname]]}/*.nc'))
+
+    # Compare maps
+    figname = f'{graphics_root}/{vname}_{test_case_name}_vs_{obs_sources[vname]}.png'
+    compare_maps.main(test_files, obs_files, vname, figname,
+            test_name=test_case_name, cntl_name=obs_sources[vname],
+            map_file=map_files[obs_sources[vname]],
+            lbAutoManage=False,
+            lbTitleFontHeightF=0.02,
+            lbLabelFontHeightF=0.02,
+            )
