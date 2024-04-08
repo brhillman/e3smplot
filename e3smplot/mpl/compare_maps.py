7a85301210948110743b0dcf141b7a2e63815f65
Benjamin Hillman <bhillma@sandia.gov>
2024-04-08 12:47:00 -0700

    Update compare_maps

--- e3smplot/mpl/compare_maps.py
+++ e3smplot/mpl/compare_maps.py
@@ -8,7 +8,7 @@ from time import perf_counter
 from scipy.interpolate import griddata
 from e3smplot.mpl.plot_maps import plot_map
 from e3smplot.plot_utils import area_average
-from e3smplot.e3sm_utils import open_dataset, get_data, get_area_weights
+from e3smplot.e3sm_utils import get_data, get_area_weights
 from e3smplot.utils import apply_map, myprint
 
 def compare_maps(coords, data_arrays, labels, figsize=None, cb_kwargs=None, **kwargs):
@@ -45,21 +45,16 @@ def compare_maps(coords, data_arrays, labels, figsize=None, cb_kwargs=None, **kw
 
     return figure
 
+def open_dataset(*inputfiles):
+    return xarray.open_mfdataset(inputfiles, use_cftime=True, data_vars='minimal', coords='minimal', compat='override')
 
-def main(varname, outputfile, testfiles, cntlfiles, t_index=None, t1=None, t2=None, maps=None, percentile=5, verbose=False, **kwargs):
+def main(varnames, outputfile, testfiles, cntlfiles,
+         t_index=None, t1=None, t2=None, mapfiles=None, percentile=5, verbose=False, **kwargs):
     #
     # Open datasets if needed (may also pass datasets directly rather than filenames)
     #
     if verbose: myprint('Open datasets...')
     datasets = [f if isinstance(f, xarray.Dataset) else open_dataset(*f) for f in (testfiles, cntlfiles)]
-    #
-    # Subset data
-    #
-    if verbose: myprint('Subset consistent time periods...')
-    if t1 is None: t1 = max([ds.time[0].values for ds in datasets])
-    if t2 is None: t2 = min([ds.time[-1].values for ds in datasets])
-    if verbose: myprint('Comparing period {} to {}'.format(str(t1), str(t2)))
-    datasets = [ds.sel(time=slice(str(t1), str(t2))) for ds in datasets]
     #
     # Compute time average
     #
@@ -74,9 +69,10 @@ def main(varname, outputfile, testfiles, cntlfiles, t_index=None, t1=None, t2=No
     # TODO: set case names
     #
     if verbose: myprint('Get data...')
-    data_arrays = [get_data(ds, varname) for ds in datasets]
-    lons = [get_data(ds, 'lon') for ds in datasets]
-    lats = [get_data(ds, 'lat') for ds in datasets]
+    data_arrays = [get_data(ds, varname) for ds, varname in zip(datasets,
+                                                                varnames)]
+    lons = [get_data(ds, 'longitude') for ds in datasets]
+    lats = [get_data(ds, 'latitude') for ds in datasets]
     #
     # Area needed for weighted average calculation
     #
@@ -86,25 +82,25 @@ def main(varname, outputfile, testfiles, cntlfiles, t_index=None, t1=None, t2=No
     # Remap if needed
     #
     if verbose: myprint('Remap to lat/lon grid if needed...')
-    if maps is not None:
-        map_datasets = [xarray.open_dataset(f) if f is not None else None for f in maps]
-        area_arrays = [apply_map(m, f)[0] if f is not None else m for (m, f) in zip(area_arrays, map_datasets)]
-        data_arrays = [apply_map(m, f)[0] if f is not None else m for (m, f) in zip(data_arrays, map_datasets)]
-
-    # redefine lons and lats after remap
-    # TODO: this is not unstructured grid-friendly
-    lons = [d.lon for d in data_arrays]
-    lats = [d.lat for d in data_arrays]
+    if mapfiles is not None:
+        map_datasets = [xarray.open_dataset(f) if f is not None else None for f in mapfiles]
+        area_arrays, lons, lats = zip(*[apply_map(m, f) if f is not
+                                   None else (m, x, y) for (m, x, y, f) in
+                                   zip(area_arrays, lons, lats, map_datasets)])
+        data_arrays, lons, lats = zip(*[apply_map(m, f) if f is not
+                                   None else (m, x, y) for (m, x, y, f) in
+                                   zip(data_arrays, lons, lats, map_datasets)])
     #
     # Compute differences
     #
     if verbose: myprint('Compute differences...')
-    data_arrays.append(data_arrays[0] - data_arrays[1])
-    area_arrays.append(area_arrays[0])
-    lons.append(lons[0])
-    lats.append(lats[0])
+    data_arrays = (*data_arrays, data_arrays[0].copy(deep=True))
+    data_arrays[-1].data = data_arrays[0].data - data_arrays[1].data
     data_arrays[-1].attrs = data_arrays[0].attrs
     data_arrays[-1].attrs['description'] = 'test minus control'
+    area_arrays = (*area_arrays, area_arrays[0])
+    lons = (*lons, lons[0])
+    lats = (*lats, lats[0])
     #
     # Pop labels out of kwargs
     #
@@ -116,14 +112,14 @@ def main(varname, outputfile, testfiles, cntlfiles, t_index=None, t1=None, t2=No
     # Make figure
     #
     if verbose: myprint('Make plots...')
-    figure, axes = pyplot.subplots(1, 3, figsize=(15, 5), subplot_kw=dict(projection=crs.PlateCarree(central_longitude=180)))
+    figure, axes = pyplot.subplots(len(data_arrays), 1, figsize=(8, 5*len(data_arrays)), subplot_kw=dict(projection=crs.PlateCarree(central_longitude=180)))
     cmaps = ['viridis', 'viridis', 'RdBu_r']
     vmin = min([numpy.nanpercentile(da.values, percentile) for da in data_arrays[:-1]])
     vmax = max([numpy.nanpercentile(da.values, 100-percentile) for da in data_arrays[:-1]])
     vmins = [vmin, vmin, -abs(data_arrays[-1].max().values)]
     vmaxs = [vmax, vmax,  abs(data_arrays[-1].max().values)]
     plots = [
-        plot_map(lons[i], lats[i], data_arrays[i], axes=axes[i], cmap=cmaps[i], vmin=vmins[i], vmax=vmaxs[i], transform=crs.PlateCarree(central_longitude=0), **kwargs)
+        plot_map(lons[i], lats[i], data_arrays[i], axes=axes[i], cmap=cmaps[i], vmin=vmins[i], vmax=vmaxs[i], **kwargs)
         for i in range(len(data_arrays))
     ]
     #
@@ -141,6 +137,7 @@ def main(varname, outputfile, testfiles, cntlfiles, t_index=None, t1=None, t2=No
     #
     # Save figure
     #
+    pyplot.tight_layout()
     figure.savefig(outputfile, bbox_inches='tight')
     pyplot.close()
 
